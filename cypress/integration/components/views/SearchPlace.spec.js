0e7308b37ac38754e618177fc9d82ebc1ab9e301
Antonio Pérez Vázquez <73110779+antoniope33@users.noreply.github.com>
2022-03-23 18:06:30 +0100

    test: Añade test de listar anuncios
    Se ha añadido el test end to end para comprobar la funcionalidad de listar anuncios.

--- cypress/integration/components/views/SearchPlace.spec.js
+++ cypress/integration/components/views/SearchPlace.spec.js
@@ -9,4 +9,11 @@ describe("SearchPlace", () => {
         cy.accessSearch();
         cy.contains("Mapa");
     });
+
+    it("list announcements succesfully", () => {
+        cy.get('div .announcements-list')
+        cy.get('div .announcement-card');
+        cy.get('div .p-datascroller-header');
+        cy.get('div .p-datascroller-content')
+    })
 })
