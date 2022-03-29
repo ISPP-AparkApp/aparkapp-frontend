642902bafe41101037593116fa63690f2d1c0924
javivm17 <javiervilarinyomayo.cr@gmail.com>
2022-03-29 11:21:06 +0200

    test: Añade test de la funcionalidad de editar anuncio
    Se han realizado pruebas end to end sobre la funcionalidad de editar un anuncio.

---
+++ cypress/integration/components/views/EditAnnouncement.spec.js
@@ -0,0 +1,27 @@
+describe("Edit", () => {
+    beforeEach(() => {
+        cy.initialOpen();
+        cy.get('a.p-menuitem-link').first().click()
+        cy.get('input[placeholder="Nombre de usuario"]').type("admin");
+        cy.get('input[placeholder="Contraseña"]').type("admin");
+        cy.get('button').click();
+    });
+    
+    it("Edit succesfuly", () => {
+        cy.contains('Actividad').click()
+        cy.contains('Editar anuncio').click()
+        cy.contains('Ubicación actual').click()
+        cy.wait(5000)
+        cy.contains('Guardar').click()
+        cy.contains("Anuncio modificado")
+    });
+
+    it("Edit unsuccessfully", () => {
+        cy.contains('Actividad').click()
+        cy.contains('Editar anuncio').click()
+        cy.get('input.p-inputtext.p-component.p-filled.p-inputnumber-input').first().clear()
+        cy.contains('Guardar').click()
+        cy.contains("Tiempo de espera requerido")
+    });
+
+})
