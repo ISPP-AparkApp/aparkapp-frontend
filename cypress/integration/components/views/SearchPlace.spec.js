25d4fa444551550cd8c2982f3ebdeb5664ed08a5
javivm17 <javiervilarinyomayo.cr@gmail.com>
2022-04-04 10:59:15 +0200

    test: Añade tests de la nueva funcionalidad de buscar y filtrar
    Se han añadido los test correspondientes a la búsqueda y filtrado de anuncios.

--- cypress/integration/components/views/SearchPlace.spec.js
+++ cypress/integration/components/views/SearchPlace.spec.js
@@ -17,4 +17,19 @@ describe("SearchPlace", () => {
         cy.get('div .p-datascroller-header');
         cy.get('div .p-datascroller-content')
     })
+
+    it("Search by location and filter", () => {
+        cy.get('input[placeholder="Busca en la zona donde quieras aparcar"]').type("Matalascañas");
+        cy.get('span.pi-search').click();
+        cy.get('span.pi-filter').click();
+        cy.contains("¿Qué día deseas aparcar?");
+        cy.contains("Escoge un rango de precios")
+        cy.contains("Aplicar").click();
+    })
+
+    it("Bad search by location", () => {
+        cy.get('input[placeholder="Busca en la zona donde quieras aparcar"]').type("sqiudhuiewqfhi");
+        cy.get('span.pi-search').click();
+        cy.contains("No se ha encontrado la ubicación");
+    })
 })
