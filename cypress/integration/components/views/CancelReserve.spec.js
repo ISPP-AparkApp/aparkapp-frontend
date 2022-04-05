adab79dc930ad7d6133ad14820890ba4046b6f72
javivm17 <javiervilarinyomayo.cr@gmail.com>
2022-04-06 00:13:48 +0200

    fix: Arregla tests

--- cypress/integration/components/views/CancelReserve.spec.js
+++ cypress/integration/components/views/CancelReserve.spec.js
@@ -2,8 +2,8 @@ describe("CancelReservation", () => {
     beforeEach(() => {
         cy.initialOpen();
         cy.get('a.p-menuitem-link').first().click();
-        cy.get('input[placeholder="Nombre de usuario"]').type("cliente1");
-        cy.get('input[placeholder="Contraseña"]').type("QWerty45");
+        cy.get('input[placeholder="Nombre de usuario"]').type("admin");
+        cy.get('input[placeholder="Contraseña"]').type("admin");
         cy.get('button').click();
     });     
 
