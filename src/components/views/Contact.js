3cf8c5e559f635bcabcd409c1867bda53a685803
Maribel Ramos <maribelrb.5@gmail.com>
2022-04-24 16:30:56 +0200

    fix: Arregla enlace términos y condiciones

--- src/components/views/Contact.js
+++ src/components/views/Contact.js
@@ -4,34 +4,41 @@ import "../../css/views/Contact.css";
 const Contact = () => {
 
     return (
-            <Card title="¡Contáctanos!" className='p-card-about text-center'>
-                <div className='grid w-full mt-5'>
-                    <div className='col-12 md:col-4'>
-                        <img src="instagram.png" alt="Icono instagram" height={60}></img>
-                        <span className='p-social-media font-bold ml-3'>@_apparkapp</span>
-                    </div>
-                    <div className='col-12 md:col-4'>
-                        <img src="twitter.png" alt="Icono twitter" height={60}></img>
-                        <span className='p-social-media font-bold ml-3'>@ApparkApp</span>
-                    </div>
-                    <div className='col-12 md:col-4'>
-                        <img src="gmail.png" alt="Icono gmail" height={60}></img>
-                        <span className='p-social-media font-bold ml-3'>aparkapp.info@gmail.com</span>
-                    </div>
+        <Card title="¡Contáctanos!" className='p-card-about text-center'>
+            <div className='grid w-full mt-5'>
+                <div className='col-12 md:col-4'>
+                    <img src="instagram.png" alt="Icono instagram" height={60}></img>
+                    <span className='p-social-media font-bold ml-3'>@_apparkapp</span>
                 </div>
-                <div className='mt-8 flex flex-column align-items-center'>
-                    <div>
-                        <span className='p-terms-conditions'>
-                            Consultar <a href="https://cutt.ly/uGuqfc3" target="_blank" rel="noreferrer" >Términos y condiciones </a> de AparkApp
-                        </span>
-                    </div>
-                    <div>
-                        <span className='p-terms-conditions'>
-                            Horario de disponibilidad: Lunes a Domingo de 8:00 AM a 2:00 AM
-                        </span>
-                    </div>
+                <div className='col-12 md:col-4'>
+                    <img src="twitter.png" alt="Icono twitter" height={60}></img>
+                    <span className='p-social-media font-bold ml-3'>@ApparkApp</span>
                 </div>
-            </Card>
+                <div className='col-12 md:col-4'>
+                    <img src="gmail.png" alt="Icono gmail" height={60}></img>
+                    <span className='p-social-media font-bold ml-3'>aparkapp.info@gmail.com</span>
+                </div>
+            </div>
+            <div className='mt-8 flex flex-column align-items-center'>
+                <div>
+                    <span className='p-terms-conditions'>
+                        Consultar
+                        <a
+                            href="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/20db1f27-07b2-4b56-beec-9065cf13a582/aparkapp_GDPR.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220424%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220424T142607Z&X-Amz-Expires=86400&X-Amz-Signature=7502743dc100987acf2614609f32215ad7ceecd12c36dd1ea46ddaf7a09560df&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22aparkapp_GDPR.pdf%22&x-id=GetObject"
+                            target="_blank"
+                            rel="noreferrer" >
+                            Términos y condiciones
+                        </a>
+                        de AparkApp
+                    </span>
+                </div>
+                <div>
+                    <span className='p-terms-conditions'>
+                        Horario de disponibilidad: Lunes a Domingo de 8:00 AM a 2:00 AM
+                    </span>
+                </div>
+            </div>
+        </Card>
     )
 }
 
