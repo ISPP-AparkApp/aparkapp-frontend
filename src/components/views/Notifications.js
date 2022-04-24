f5cc8208fd0a0a60db2df1261e91e26421b09f74
ferherque <ferherque@alum.us.es>
2022-04-24 19:34:52 +0200

    fix: Reemplaza notificaciones
    Se ha eliminado el aviso al cancelar, ya que se va
    a realizar en actividades

--- src/components/views/Notifications.js
+++ src/components/views/Notifications.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Button } from "primereact/button";
 import { Card } from 'primereact/card';
 import { Avatar } from 'primereact/avatar';
-import { ProgressSpinner } from 'primereact/progressspinner';
-import { getAnnouncement, updateStatusAnnouncement, getReservationUser } from '../../api/api';
+import { getAnnouncement, updateStatusAnnouncement, getReservationUser, transaction } from '../../api/api';
 import "../../css/views/Notifications.css";
+import { ProgressSpinner } from 'primereact/progressspinner';
 
 const Notifications = () => {
     const [announcement, setAnnouncement] = useState();
@@ -16,7 +16,7 @@ const Notifications = () => {
     const getAnnounce = () => {
         getAnnouncement(announceId).then(val => {
             setAnnouncement(val);
-            if (!user) { getReservationUser(val.id).then(u => u ? setUser(u.first_name) : setUser("Cancelled")); }
+            if (!user) { getReservationUser(val.id).then(u => setUser(u.first_name)); }
         });
     };
 
@@ -35,6 +35,7 @@ const Notifications = () => {
     }, [announcement, time, user]);
 
     const updateAnnounce = (status) => { updateStatusAnnouncement(announcement.id, { status: status }).then(getAnnounce()); }
+    const transferAmount = () => { transaction(announcement.id) };
 
     const notificationsContent = () => {
         let h = ""
@@ -42,18 +43,18 @@ const Notifications = () => {
         let f = ""
         let result = []
 
-        if (announcement && user && user !== "Cancelled") {
+        if (announcement) {
             switch (announcement.status) {
                 case "Delay":
                     h = <span className="pi pi-clock text-xl">   {user} se ha retrasado</span>;
                     c = <div><p className="mb-0 text-xl"><Avatar style={{ color: "white" }} icon="pi pi-user" />   Llego tarde</p><p className="text-xs">Aceptar supone esperar 5 minutos más y recibir 50 céntimos más sobre el precio final</p><br></br><hr></hr></div>
-                    f = <div><div><Button onClick={() => { updateAnnounce("AcceptDelay"); }} className="p-button-raised p-button-lg mb-3 w-full h-full" label="Vale, te espero" /></div><div><Button onClick={() => { updateAnnounce("DenyDelay"); }} className="p-button-raised p-button-lg w-full h-full" label="Lo siento, me voy" /></div></div>
+                    f = <div><div><Button onClick={() => { transferAmount(); }} className="p-button-raised p-button-lg mb-3 w-full h-full" label="Vale, te espero" /></div><div><Button onClick={() => { updateAnnounce("DenyDelay"); }} className="p-button-raised p-button-lg w-full h-full" label="Lo siento, me voy" /></div></div>
                     result = [h, c, f];
                     break;
                 case "Arrival":
                     h = <span className="pi pi-map-marker text-xl">   {user} está en tu ubicación</span>;
                     c = <div><p className="mb-0 text-xl"><Avatar style={{ color: "white" }} icon="pi pi-user" />   ¡He llegado!</p><br></br><hr></hr></div>
-                    f = <span><Button onClick={() => { updateAnnounce("Departure"); }} className="p-button-raised p-button-lg w-full h-full" label="Perfecto, salgo" /></span>
+                    f = <span><Button onClick={() => updateAnnounce("Departure") } className="p-button-raised p-button-lg w-full h-full" label="Perfecto, salgo" /></span>
                     result = [h, c, f];
                     break;
                 default:
@@ -61,17 +62,9 @@ const Notifications = () => {
                     result = [h, c, f];
                     break;
             }
-        } else if(user === "Cancelled") {
-            h = "Reserva cancelada"
-            c = <p className="mb-0 text-xl">Puede salir de su plaza de aparcamiento cuando desee</p>
-            result = [h, c, f]
         }
         else {
-            c = <ProgressSpinner
-            style={{ width: "50px", height: "50px" }}
-            strokeWidth="3"
-            animationDuration=".5s"
-          />
+            c = <ProgressSpinner />
             result = [h, c, f];
         }
         return result;
