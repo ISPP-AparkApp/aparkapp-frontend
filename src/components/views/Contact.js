2e9d5e91251092b3ef856beb277e64e94eb6086d
Maribel Ramos <maribelrb.5@gmail.com>
2022-04-24 17:10:57 +0200

    Fix: Espacios entre palabras

--- src/components/views/Contact.js
+++ src/components/views/Contact.js
@@ -22,14 +22,14 @@ const Contact = () => {
             <div className='mt-8 flex flex-column align-items-center'>
                 <div>
                     <span className='p-terms-conditions'>
-                        Consultar
+                        Consultar &nbsp;
                         <a
                             href="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/20db1f27-07b2-4b56-beec-9065cf13a582/aparkapp_GDPR.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220424%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220424T142607Z&X-Amz-Expires=86400&X-Amz-Signature=7502743dc100987acf2614609f32215ad7ceecd12c36dd1ea46ddaf7a09560df&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22aparkapp_GDPR.pdf%22&x-id=GetObject"
                             target="_blank"
                             rel="noreferrer" >
                             Términos y condiciones
                         </a>
-                        de AparkApp
+                        &nbsp; de AparkApp
                     </span>
                 </div>
                 <div>
